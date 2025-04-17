8247782005176504d86fff0a76046df97aac476a
DhammikaKekulawala <dhammika96815@gmail.com>
2025-04-17 18:04:51 +0530

    create chunk

--- app/service/document_service.py
+++ app/service/document_service.py
@@ -1,7 +1,11 @@
 from PyPDF2 import PdfReader
 from fastapi import UploadFile
+from app.utils.create_chunk import CreateChunk
 
 class DocumentService:
+    def __init__(self):
+        self.create_chunk = CreateChunk()
+        
     def get_text(self, file: UploadFile) -> str:
         """Extract text from PDF file."""
         pdf_reader = PdfReader(file.file)
